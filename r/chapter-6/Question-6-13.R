409d09f495bfc99ccb49bb1bfb72251b5a94de00
nbclsc <nbclsc@unm.edu>
2024-12-28 23:21:54 -0700

    R code for Exercise 6.13.

---
+++ r/chapter-6/Question-6-13.R
@@ -0,0 +1,15 @@
+# Make sure the treatment column are factor variables.
+# Otherwise the output will not match.
+df <- readxl::read_excel("../../data/Exercise6.13.xlsx") |>
+    dplyr::mutate(Factor1 = as.factor(Factor1),
+                  Factor2 = as.factor(Factor2))
+
+# Use the car package for MANOVA. Personally, I'd rather use R for this
+# than Python. Python results using statsmodels.multivariate.manova don't
+# match the book (output here).
+fit_q_6_13 <- car::Manova(lm(cbind(x1, x2) ~ Factor1 + Factor2, data = df))
+
+summary(fit_q_6_13)
+
+# fit$SSP  # B matrices
+# fit$SSPE  # W matrix
